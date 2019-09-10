fdab8a4eae72a5bd77ebc2cec24ad83a2c8db390
James Martin <james@jtmar.me>
2019-09-09 17:22:04 -0700

    Added soft dependency on Towny to add town tags as chat prefixes.

--- src/me/jtmar/wasteland/listeners/ChatListener.java
+++ src/me/jtmar/wasteland/listeners/ChatListener.java
@@ -5,14 +5,20 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-
+import me.jtmar.wasteland.TownyDependency;
+import me.jtmar.wasteland.Wasteland;
 import me.jtmar.wasteland.ranks.EnlistedRank;
 
 public class ChatListener implements Listener {
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerChat(AsyncPlayerChatEvent event) {
+		String townyPrefix = "";
+		if (Wasteland.getInstance().getServer().getPluginManager().isPluginEnabled("Towny")) {
+			townyPrefix = TownyDependency.getTownTagPrefix(event.getPlayer());
+		}
 		event.setFormat(
-				"[" + EnlistedRank.getRank(event.getPlayer()).format() + ChatColor.RESET + "] " + " "
-				+ "%s" + ChatColor.WHITE + ": %s");
+				townyPrefix
+					+ "[" + EnlistedRank.getRank(event.getPlayer()).format() + ChatColor.RESET + "] " + " "
+					+ "%s" + ChatColor.WHITE + ": %s");
 	}
 }
